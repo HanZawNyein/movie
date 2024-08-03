349ef3fd79a43478a2c710d6f4d143f078522be7
Agga <agga@Aggas-MacBook-Air.local>
2024-08-03 11:29:06 +0700

    seperate data file

--- public/store.js
+++ public/store.js
@@ -1,63 +1,9 @@
 import {reactive, useEnv, useState} from "@odoo/owl";
+import {MOVIES} from "./data.js";
 
 class MovieList {
     nextId = 10;
-    movies = [
-        {
-            "id": 1,
-            "title": "JJ",
-            "release": "2024",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-        {
-            "id": 2,
-            "title": "Deadpool",
-            "release": "2023",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-        {
-            "id": 3,
-            "title": "Deadpool",
-            "release": "2023",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-        {
-            "id": 4,
-            "title": "Deadpool",
-            "release": "2023",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-        {
-            "id": 5,
-            "title": "Deadpool",
-            "release": "2023",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-        {
-            "id": 6,
-            "title": "Deadpool",
-            "release": "2023",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-        {
-            "id": 7,
-            "title": "Deadpool",
-            "release": "2023",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-        {
-            "id": 8,
-            "title": "Deadpool",
-            "release": "2023",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-        {
-            "id": 9,
-            "title": "Deadpool",
-            "release": "2023",
-            "cover": "https://images.pexels.com/photos/675920/pexels-photo-675920.jpeg?cs=srgb&dl=pexels-minan1398-675920.jpg&fm=jpg"
-        },
-    ];
+    movies =MOVIES;
 
     addMovie(newMovie) {
         const task = {
